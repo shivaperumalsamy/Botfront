03016d103607dae2622283cbcc723e13dbf758ac
Sharath Ravichandran <sharath.ravichandran@siriuscom.com>
2021-10-21 20:09:19 +0530

    UTCOURT-987: handled case when the access token is deleted

--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -497,7 +497,7 @@ class Widget extends Component {
                         'QBqa5nInszTruDUQayoEokoF08jW%2FnK6YhM55BahiyQRO2NkM6oSlvIBC%2Bio6iCznxH11lucohTi%0D%0ArWN4FbU6PeYqOIRd4PNeDXuhk%2FXNDpN4C1o9p88saGEu%2BctB%2Bhh6Fjdgw60AyudMD8Kl45JUNR%2BC%0D%0AOMIfyyJ4yIOBqCcuGtlSXyw2kjM2NfH48TIEQVEh05POk7Z3IMHaDOHiSWs05pqVJxPVL2Sq5FX4%0D%0AjZ97hZ3XFwzEd9gHJn5d0v3nfFShVq6IxqyFy3Gmr%2Bbc8f6M8nq99XUGAAb7tonMjSDyACo%3D';
 
                     socket.emit('user_uttered', {
-                        message: initPayload || 'welcome user',
+                        message: initPayload || '/welcome',
                         customData,
                         session_id: sessionId,
                     });
@@ -506,7 +506,7 @@ class Widget extends Component {
                 customData.auth['token'] =
                     'QBqa5nInszTruDUQayoEokoF08jW%2FnK6YhM55BahiyQRO2NkM6oSlvIBC%2Bio6iCznxH11lucohTi%0D%0ArWN4FbU6PeYqOIRd4PNeDXuhk%2FXNDpN4C1o9p88saGEu%2BctB%2Bhh6Fjdgw60AyudMD8Kl45JUNR%2BC%0D%0AOMIfyyJ4yIOBqCcuGtlSXyw2kjM2NfH48TIEQVEh05POk7Z3IMHaDOHiSWs05pqVJxPVL2Sq5FX4%0D%0AjZ97hZ3XFwzEd9gHJn5d0v3nfFShVq6IxqyFy3Gmr%2Bbc8f6M8nq99XUGAAb7tonMjSDyACo%3D';
                 socket.emit('user_uttered', {
-                    message: initPayload || 'welcome user',
+                    message: initPayload || '/welcome',
                     customData,
                     session_id: sessionId,
                 });
