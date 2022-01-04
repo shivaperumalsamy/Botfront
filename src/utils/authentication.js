08d9f9cbe2630fb53c5595d58032091e5aeac7be
Sharath Ravichandran <sharath.ravichandran@siriuscom.com>
2022-01-04 15:02:43 +0530

    UTCOURT-1379: updated switch condition

--- src/utils/authentication.js
+++ src/utils/authentication.js
@@ -3,6 +3,21 @@ import { post } from './fetch';
 function authenticate(customData) {
     if ('token' in customData) {
         return new Promise((resolve, reject) => {
+            post(customData.authUrl, {
+                token: customData.token,
+            })
+                .then((response) => {
+                    customData['accessToken'] = response.accessToken;
+                    sessionStorage.setItem('ACCESS_TOKEN', customData['accessToken']);
+                    resolve();
+                })
+                .catch((error) => {
+                    if ('accessToken' in customData) delete customData['accessToken'];
+                    reject();
+                });
+        });
+    } else {
+        return new Promise((resolve, reject) => {
             window.getChatbotAuthenticationToken((data) => {
                 if (!data.error) {
                     post(customData.authUrl, {
@@ -22,21 +37,6 @@ function authenticate(customData) {
                     reject();
                 }
             });
-        });
-    } else {
-        return new Promise((resolve, reject) => {
-            post(customData.authUrl, {
-                token: customData.token,
-            })
-                .then((response) => {
-                    customData['accessToken'] = response.accessToken;
-                    sessionStorage.setItem('ACCESS_TOKEN', customData['accessToken']);
-                    resolve();
-                })
-                .catch((error) => {
-                    if ('accessToken' in customData) delete customData['accessToken'];
-                    reject();
-                });
         });
     }
 }
